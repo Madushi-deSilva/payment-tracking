a1d30064e76a8655e4905746c4e6b2e9a9656fac
Madushi-deSilva <madushi97pabasara@gmail.com>
2021-07-21 16:52:52 +0530

    generate reports

---
+++ src/components/reports/clientreport.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Homenav from '../Homenav';
+import Sidebar from '../Sidebar';
+
+function ClientReport() {
+        return ( 
+            <div>
+                <Homenav/>
+                <div className="row" style={{maxWidth:'100%'}}>
+                    <Sidebar/>
+                    {/* ---------------reports page------------ */}
+                    
+                    <div className="homeDiv col-10 text-center" style={{height:'88vh'}}>
+                        <iframe className="mt-5" width="1200" height="550" src="https://app.powerbi.com/reportEmbed?reportId=2bf71ba9-f9ac-4be9-ba4d-0402e6e758db&autoAuth=true&ctid=aa232db2-7a78-4414-a529-33db9124cba7&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+                        <div className="row form-group mx-3 mt-2 formGroup">
+                            <div className='col text-center'>
+                                <button name="Save" value="Save" type="submit" className="btn btn-primary custom-btn1 ml-1">SAVE</button>
+                                <Link to="/reports">
+                                    <button name="Cancel" value="Cancel" type="submit" className="btn btn-primary custom-btn2 ml-1">CANCEL</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+         );
+}
+ 
+export default ClientReport;
---
+++ src/components/reports/overall.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Homenav from '../Homenav';
+import Sidebar from '../Sidebar';
+
+function OverallReport() {
+        return ( 
+            <div>
+                <Homenav/>
+                <div className="row" style={{maxWidth:'100%'}}>
+                    <Sidebar/>
+                    {/* ---------------reports page------------ */}
+                    
+                    <div className="homeDiv col-10 text-center" style={{height:'88vh'}}>
+                        <iframe className="mt-5" width="1200" height="550" src="https://app.powerbi.com/reportEmbed?reportId=5c5866ba-ee23-4c28-a43d-dd967eef36ff&autoAuth=true&ctid=aa232db2-7a78-4414-a529-33db9124cba7&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+                        <div className="row form-group mx-3 mt-2 formGroup">
+                            <div className='col text-center'>
+                                <button name="Save" value="Save" type="submit" className="btn btn-primary custom-btn1 ml-1">SAVE</button>
+                                <Link to="/reports">
+                                    <button name="Cancel" value="Cancel" type="submit" className="btn btn-primary custom-btn2 ml-1">CANCEL</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+         );
+}
+ 
+export default OverallReport;
