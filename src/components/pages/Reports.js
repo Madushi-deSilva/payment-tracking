d2fd0dbaf4b1a7b39c3d166d65d56ddb718947de
Madushi-deSilva <madushi97pabasara@gmail.com>
2021-07-21 16:52:01 +0530

    modify interface

--- src/components/pages/Reports.js
+++ src/components/pages/Reports.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom'
+import {Card, CardGroup } from 'react-bootstrap'
 // import '../../App.css'
 import Homenav from '../Homenav';
 import Sidebar from '../Sidebar'
@@ -21,41 +21,46 @@ class Reports extends Component {
                             <h1 style={{fontFamily:'serif', padding:'10px'}}>REPORTS</h1>
                         </div>
                         <div className="row" style={{margin:'10px'}}>
-                            <div className="col">
+                        <CardGroup>
+                            <Card className="mr-2 ml-2" style={{border:'1px', borderRadius:'5px', backgroundColor:'lightblue'}}>
+                                <Card.Body>
+                                    <Card.Link href="/client-report" style={{color:'black', fontFamily:'Georgia', fontSize:'20px'}}>Client Payment Report</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card className="mr-2" style={{border:'1px', borderRadius:'5px', backgroundColor:'lightgreen'}}>
+                                <Card.Body>
+                                    <Card.Link href="/due-report" style={{color:'black', fontFamily:'Georgia', fontSize:'20px'}}>Due Payments Report</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card className="mr-2" style={{border:'1px', borderRadius:'5px', backgroundColor:'yellow'}}>
+                                <Card.Body>
+                                    <Card.Link href="/overdue-report" style={{color:'black', fontFamily:'Georgia', fontSize:'20px'}}>Overdue Payments Report</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card className="mr-2" style={{border:'1px', borderRadius:'5px', backgroundColor:'violet'}}>
+                                <Card.Body>
+                                    <Card.Link href="/received-report" style={{color:'black', fontFamily:'Georgia', fontSize:'20px'}}>Received Payments Report</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card className="mr-2" style={{border:'1px', borderRadius:'5px', backgroundColor:'orange'}}>
+                                <Card.Body>
+                                    <Card.Link href="/overall-report" style={{color:'black', fontFamily:'Georgia', fontSize:'20px'}}>Overall Analysis Report</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            </CardGroup>
+                            {/* <div className="col">
                             <div className="form-group row formGroup">
                                     <label for="name" className="col-12 col-md-4 col-xl-4 lblRight">Category</label>
                                     <select className="form-control form-control-sm col-12 col-md-8 col-xl-8" id="category">
-                                            <option value="client">Client payment report</option>
+                                            <option value="client" href="/due-payments">Client payment report</option>
                                             <option value="due">Due payments</option>
                                             <option value="overdue">Overdue payments</option>
                                             <option value="received">Received payments</option>
                                             <option value="overall">Overall analysis</option>
                                     </select>
                                 </div>
-                            </div>
-                            <div className="col reportCol">
-                                <div className="form-group row formGroup">
-                                    <label for="name" className="col-12 col-md-4 col-xl-4 lblRight">From</label>
-                                    <input type="date" className="form-control form-control-sm col-12 col-md-8 col-xl-8" id="from"
-                                        name="from" required/>
-                                </div>
-                                <div className="form-group row formGroup">
-                                    <label for="name" className="col-12 col-md-4 col-xl-4 lblRight">To</label>
-                                    <input type="date" className="form-control form-control-sm col-12 col-md-8 col-xl-8" id="to"
-                                        name="to" required/>
-         
-                                </div>
-                            </div>
+                            </div> */}
                         
-                        </div>
-                        <div className="row" style={{margin:'10px', textAlign:'right'}}>
-                            <div className="col">
-                                <div className="row form-group mx-3 formGroup">
-                                    <Link>
-                                        <button name="send" value="send" type="submit" className="btn btn-success ml-1 btnPreview">PREVIEW</button>
-                                    </Link>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
