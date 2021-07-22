79e022ebe4078a31c7049cb1b690c5835abaa0c0
Madushi-deSilva <madushi97pabasara@gmail.com>
2021-07-22 23:50:07 +0530

    add comments

--- src/components/pages/OverduePayments.js
+++ src/components/pages/OverduePayments.js
@@ -2,7 +2,6 @@ import React, {useState,useEffect} from 'react';
 import {Button, Modal} from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import Axios from 'axios'
-// import '../../App.css
 import Homenav from '../Homenav';
 import Sidebar from '../Sidebar'
 import Table from 'react-bootstrap/Table'
@@ -12,22 +11,22 @@ function OverduePayments() {
     const [show, setShow] = useState(false);
     const [deleteID, setdeleteID] = useState('');
 
-  const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
-const handleShow = (id) => {
-    setShow(true);
-    setdeleteID(id)
+    const handleClose = () => setShow(false);
+    const handleShow = (id) => {
+        setShow(true);
+        setdeleteID(id)
 
-}
-//delete payment by id
-const deleteOverdue = (id) =>{
-    console.log("Del id",id)
-    console.log("Del status")
-    Axios.delete(`http://localhost:3001/overduepayments/delete/${id}`).then(() => {
-        console.log("deleted");
-        window.location.href = 'http://localhost:3000/overdue-payments';
-    });
-};
+    }
+
+    //delete payment by id
+    const deleteOverdue = (id) =>{
+        console.log("Del id",id)
+        console.log("Del status")
+        Axios.delete(`http://localhost:3001/overduepayments/delete/${id}`).then(() => {
+            console.log("deleted");
+            window.location.href = 'http://localhost:3000/overdue-payments';
+        });
+    };
 
     const [overdueList, setOverdueList]= useState([]);
   
@@ -40,70 +39,76 @@ const deleteOverdue = (id) =>{
            .catch((error)=>{
                console.log(error);
            })
-  },[]);
+    },[]);
 
-        return ( 
-            <div>
-                <Homenav/>
-                <div className="row" style={{maxWidth:'100%'}}>
-                    <Sidebar/>
-                    {/* ---------------overdue payments------------ */}
-                    
-                    <div className="homeDiv col-10 text-center" style={{height:'88vh'}}>
-                        <div className="row">
-                            <h1 style={{fontFamily:'serif', padding:'10px'}}>OVERDUE PAYMENTS</h1>
-                        </div>
-                        <div className="row" style={{margin:'10px'}}>
-                            <Table responsive hover>
-                                <thead style={{backgroundColor:'pink', borderTop:'2px solid black'}}> 
-                                    <tr>
-                                        <th>Overdue ID</th>
-                                        <th>Company Name</th>
-                                        <th>Invoice</th>
-                                        <th>Telephone No.</th>
-                                        <th>Company Email</th>
-                                        <th>Amount</th>
-                                        <th>Collected</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                {overdueList.map(val=>{
-                                    return(
-                                    <tr key={val.overdue_ID}>
-                                        <td>{val.overdue_ID}</td>
-                                        <td>{val.company_name}</td>
-                                        <td>{val.invoice}</td>
-                                        <td>{val.tel_no}</td>
-                                        <td>{val.email}</td>
-                                        <td>{val.amount}</td>
-                                        <td><input type="checkbox" checked={val.collected_status} id="" name="" style={{width:'20px', height:'20px'}}/></td>
-                                        <td>
-                                            <Link to={`/edit-overdue/${val.overdue_ID}`}>
-                                                <button name="view" value="view" type="submit" className="btn btn-primary ml-1 btnView"><i class="fas fa-eye" data-toggle="tooltip" data-placement="top" title="View"></i></button>
-                                            </Link>
-                                            <Link to={`/overdue-mail/${val.overdue_ID}`}>
-                                                <button name="view" value="view" type="submit" className="btn btn-success ml-1 btnView"><i class="fas fa-envelope" data-toggle="tooltip" data-placement="top" title="Send Email"></i></button>
-                                            </Link>
-                                            <Button variant="danger" onClick={() => handleShow(val.overdue_ID)}> <i class="fas fa-trash-alt" data-toggle="tooltip" data-placement="top" title="Delete"></i> </Button>
-                                            <Modal show={show} onHide={handleClose}>
-                                                <Modal.Body>Are you want to delete?</Modal.Body>
-                                                <Modal.Footer>
-                                                <Button variant="secondary" onClick={handleClose}> No </Button>
-                                                <Button variant="primary" onClick={() => deleteOverdue(deleteID)}> Yes </Button>
-                                                </Modal.Footer>
-                                            </Modal>
-                                        </td>
-                                    </tr>
-                                  );
-                                })} 
-                                </tbody>
-                            </Table>
-                        </div>
+    return ( 
+        <div>
+
+            {/* ----------navigation inserted---------------- */}
+            <Homenav/>
+
+            <div className="row" style={{maxWidth:'100%'}}>
+
+                {/* ----------sidebar inserted---------------- */}
+                <Sidebar/>
+                
+                {/* ---------------overdue payments------------ */}
+                
+                <div className="homeDiv col-10 text-center" style={{height:'88vh'}}>
+                    <div className="row">
+                        <h1 style={{fontFamily:'serif', padding:'10px'}}>OVERDUE PAYMENTS</h1>
+                    </div>
+                    <div className="row" style={{margin:'10px'}}>
+                        <Table responsive hover>
+                            <thead style={{backgroundColor:'pink', borderTop:'2px solid black'}}> 
+                                <tr>
+                                    <th>Overdue ID</th>
+                                    <th>Company Name</th>
+                                    <th>Invoice</th>
+                                    <th>Telephone No.</th>
+                                    <th>Company Email</th>
+                                    <th>Amount</th>
+                                    <th>Collected</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {overdueList.map(val=>{
+                                return(
+                                <tr key={val.overdue_ID}>
+                                    <td>{val.overdue_ID}</td>
+                                    <td>{val.company_name}</td>
+                                    <td>{val.invoice}</td>
+                                    <td>{val.tel_no}</td>
+                                    <td>{val.email}</td>
+                                    <td>{val.amount}</td>
+                                    <td><input type="checkbox" checked={val.collected_status} id="" name="" style={{width:'20px', height:'20px'}}/></td>
+                                    <td>
+                                        <Link to={`/edit-overdue/${val.overdue_ID}`}>
+                                            <button name="view" value="view" type="submit" className="btn btn-primary ml-1 btnView"><i class="fas fa-eye" data-toggle="tooltip" data-placement="top" title="View"></i></button>
+                                        </Link>
+                                        <Link to={`/overdue-mail/${val.overdue_ID}`}>
+                                            <button name="view" value="view" type="submit" className="btn btn-success ml-1 btnView"><i class="fas fa-envelope" data-toggle="tooltip" data-placement="top" title="Send Email"></i></button>
+                                        </Link>
+                                        <Button variant="danger" onClick={() => handleShow(val.overdue_ID)}> <i class="fas fa-trash-alt" data-toggle="tooltip" data-placement="top" title="Delete"></i> </Button>
+                                        <Modal show={show} onHide={handleClose}>
+                                            <Modal.Body>Are you want to delete?</Modal.Body>
+                                            <Modal.Footer>
+                                            <Button variant="secondary" onClick={handleClose}> No </Button>
+                                            <Button variant="primary" onClick={() => deleteOverdue(deleteID)}> Yes </Button>
+                                            </Modal.Footer>
+                                        </Modal>
+                                    </td>
+                                </tr>
+                                );
+                            })} 
+                            </tbody>
+                        </Table>
                     </div>
                 </div>
             </div>
-         );
+        </div>
+    );
 }
  
 export default OverduePayments;
--- src/components/pages/ReceivedPayments.js
+++ src/components/pages/ReceivedPayments.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Axios from 'axios'
 import { Link } from 'react-router-dom'
-// import '../../App.css
 import Homenav from '../Homenav';
 import Sidebar from '../Sidebar'
 import Table from 'react-bootstrap/Table'
@@ -38,7 +37,7 @@ function ReceivedPayments(){
             })
     },[]);
 
-    //update button in received due payments
+    //update button in received due payments (put a tick in  collected column in due payments)
     const updateDue=(id)=>{
         Axios.put(`http://localhost:3001/receivedpayments/receiveddue/update/${id}`)
             .then(response => {
@@ -50,7 +49,7 @@ function ReceivedPayments(){
             })
     }
 
-    //update button in received overdue payments
+    //update button in received overdue payments (put a tick in  collected column in overdue payments)
     const updateOverdue=(id)=>{
         Axios.put(`http://localhost:3001/receivedpayments/receivedoverdue/update/${id}`)
             .then(response => {
@@ -62,123 +61,129 @@ function ReceivedPayments(){
             })
     }
 
-        return ( 
-            <div>
-                <Homenav/>
-                <div className="row" style={{maxWidth:'100%'}}>
-                    <Sidebar/>
-                     {/* ---------------received payments------------ */}
-                    
-                     <div className="homeDiv col-10 text-center" style={{height:'88vh'}}>
-                        <div className="row">
-                            <h1 style={{fontFamily:'serif', padding:'10px'}}>RECEIVED PAYMENTS</h1>
-                        </div>
-                        <div className="row" style={{margin:'10px'}}>
-                        <div className="bloc-tabs">
-                            <button
-                            className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-                            onClick={() => toggleTab(1)}
-                            >
-                            <h3>Due Payments</h3>
-                            </button>
-                            <button
-                            className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-                            onClick={() => toggleTab(2)}
-                            >
-                            <h3>Overdue Payments</h3>
-                            </button>
-                            
-                        </div>
-                        {/* ----------tab content---------------- */}
-                        <div className="content-tabs">
+    return ( 
+        <div>
 
-                            {/* --------- due payments---------------- */}
-                            <div
-                            className={toggleState === 1 ? "content  active-content" : "content"}
-                            >
-                            <Table responsive hover>
-                                <thead style={{backgroundColor:'pink', borderTop:'2px solid black'}}> 
-                                    <tr>
-                                        <th>Received ID</th>
-                                        <th>Company Name</th>
-                                        <th>Invoice</th>
-                                        <th>Telephone No.</th>
-                                        <th>Company Email</th>
-                                        <th>Amount</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                {dueList.map(val=>{
-                                    return(
-                                    <tr  key={val.receivedDue_ID}>
-                                        <td>{val.receivedDue_ID}</td>
-                                        <td>{val.company_name}</td>
-                                        <td>{val.invoice}</td>
-                                        <td>{val.tel_no}</td>
-                                        <td>{val.email}</td>
-                                        <td>{val.amount}</td>
-                                        <td>
-                                            <Link>
-                                                <button name="view" onClick={() => updateDue(val.due_ID)} value="view" type="submit" className="btn btn-primary ml-1 "><i class="fas fa-edit" data-toggle="tooltip" data-placement="top" title="Update"></i></button>
-                                            </Link>
-                                            <Link to={`/received-mail/${val.receivedDue_ID}`}>
-                                            <button name="view" value="view" type="submit" className="btn btn-success ml-1 btnView"><i class="fas fa-envelope" data-toggle="tooltip" data-placement="top" title="Send Email"></i></button>
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                    );
-                                })}
-                                </tbody>
-                            </Table>
-                            </div>
+            {/* ----------navigation bar inserted---------------- */}
+            <Homenav/>
+            
+            <div className="row" style={{maxWidth:'100%'}}>
 
-                            {/* ---------- overdue payments---------------- */}
-                            <div
-                            className={toggleState === 2 ? "content  active-content" : "content"}
-                            >
-                            <Table responsive hover>
-                                <thead style={{backgroundColor:'pink', borderTop:'2px solid black'}}> 
-                                    <tr>
-                                        <th>Received ID</th>
-                                        <th>Company Name</th>
-                                        <th>Invoice</th>
-                                        <th>Telephone No.</th>
-                                        <th>Company Email</th>
-                                        <th>Amount</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                {overdueList.map(val=>{
-                                    return(
-                                    <tr key={val.receivedOverdue_ID}>
-                                        <td>{val.receivedOverdue_ID}</td>
-                                        <td>{val.company_name}</td>
-                                        <td>{val.invoice}</td>
-                                        <td>{val.tel_no}</td>
-                                        <td>{val.email}</td>
-                                        <td>{val.amount}</td>
-                                        <td>
-                                            <Link>
-                                                <button name="view" onClick={() => updateOverdue(val.overdue_ID)} value="view" type="submit" className="btn btn-primary ml-1 "><i class="fas fa-edit" data-toggle="tooltip" data-placement="top" title="Update"></i></button>
-                                            </Link>
-                                            <Link to={`/received-mail/${val.receivedOverdue_ID}`}>
-                                            <button name="view" value="view" type="submit" className="btn btn-success ml-1 btnView"><i class="fas fa-envelope" data-toggle="tooltip" data-placement="top" title="Send Email"></i></button>
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                    );
-                                })}
-                                </tbody>
-                            </Table>
-                            </div>
+                {/* ----------sidebar inserted---------------- */}
+                <Sidebar/>
+                
+                    {/* ---------------received payments------------ */}
+                
+                    <div className="homeDiv col-10 text-center" style={{height:'88vh'}}>
+                    <div className="row">
+                        <h1 style={{fontFamily:'serif', padding:'10px'}}>RECEIVED PAYMENTS</h1>
+                    </div>
+                    <div className="row" style={{margin:'10px'}}>
+                    <div className="bloc-tabs">
+                        <button
+                        className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+                        onClick={() => toggleTab(1)}
+                        >
+                        <h3>Due Payments</h3>
+                        </button>
+                        <button
+                        className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+                        onClick={() => toggleTab(2)}
+                        >
+                        <h3>Overdue Payments</h3>
+                        </button>
+                        
+                    </div>
+                    {/* ----------tab content---------------- */}
+                    <div className="content-tabs">
+
+                        {/* --------- received due payments---------------- */}
+                        <div
+                        className={toggleState === 1 ? "content  active-content" : "content"}
+                        >
+                        <Table responsive hover>
+                            <thead style={{backgroundColor:'pink', borderTop:'2px solid black'}}> 
+                                <tr>
+                                    <th>Received ID</th>
+                                    <th>Company Name</th>
+                                    <th>Invoice</th>
+                                    <th>Telephone No.</th>
+                                    <th>Company Email</th>
+                                    <th>Amount</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {dueList.map(val=>{
+                                return(
+                                <tr  key={val.receivedDue_ID}>
+                                    <td>{val.receivedDue_ID}</td>
+                                    <td>{val.company_name}</td>
+                                    <td>{val.invoice}</td>
+                                    <td>{val.tel_no}</td>
+                                    <td>{val.email}</td>
+                                    <td>{val.amount}</td>
+                                    <td>
+                                        <Link>
+                                            <button name="view" onClick={() => updateDue(val.due_ID)} value="view" type="submit" className="btn btn-primary ml-1 "><i class="fas fa-edit" data-toggle="tooltip" data-placement="top" title="Update"></i></button>
+                                        </Link>
+                                        <Link to={`/received-mail/${val.receivedDue_ID}`}>
+                                        <button name="view" value="view" type="submit" className="btn btn-success ml-1 btnView"><i class="fas fa-envelope" data-toggle="tooltip" data-placement="top" title="Send Email"></i></button>
+                                        </Link>
+                                    </td>
+                                </tr>
+                                );
+                            })}
+                            </tbody>
+                        </Table>
                         </div>
+
+                        {/* ----------received overdue payments---------------- */}
+                        <div
+                        className={toggleState === 2 ? "content  active-content" : "content"}
+                        >
+                        <Table responsive hover>
+                            <thead style={{backgroundColor:'pink', borderTop:'2px solid black'}}> 
+                                <tr>
+                                    <th>Received ID</th>
+                                    <th>Company Name</th>
+                                    <th>Invoice</th>
+                                    <th>Telephone No.</th>
+                                    <th>Company Email</th>
+                                    <th>Amount</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {overdueList.map(val=>{
+                                return(
+                                <tr key={val.receivedOverdue_ID}>
+                                    <td>{val.receivedOverdue_ID}</td>
+                                    <td>{val.company_name}</td>
+                                    <td>{val.invoice}</td>
+                                    <td>{val.tel_no}</td>
+                                    <td>{val.email}</td>
+                                    <td>{val.amount}</td>
+                                    <td>
+                                        <Link>
+                                            <button name="view" onClick={() => updateOverdue(val.overdue_ID)} value="view" type="submit" className="btn btn-primary ml-1 "><i class="fas fa-edit" data-toggle="tooltip" data-placement="top" title="Update"></i></button>
+                                        </Link>
+                                        <Link to={`/received-mail/${val.receivedOverdue_ID}`}>
+                                        <button name="view" value="view" type="submit" className="btn btn-success ml-1 btnView"><i class="fas fa-envelope" data-toggle="tooltip" data-placement="top" title="Send Email"></i></button>
+                                        </Link>
+                                    </td>
+                                </tr>
+                                );
+                            })}
+                            </tbody>
+                        </Table>
                         </div>
+                    </div>
                     </div>
                 </div>
             </div>
-         );
+        </div>
+    );
 }
  
 export default ReceivedPayments;
