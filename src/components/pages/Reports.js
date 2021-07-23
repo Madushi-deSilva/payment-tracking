f0dc8516cd1af56422b947d654a7da51e279ca31
Madushi-deSilva <madushi97pabasara@gmail.com>
2021-07-23 18:42:26 +0530

    report print function added

--- src/components/pages/Reports.js
+++ src/components/pages/Reports.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Card, CardGroup } from 'react-bootstrap'
+import {Card, CardGroup, Container } from 'react-bootstrap'
 import { Link } from "react-router-dom";
 import Homenav from '../Homenav';
 import Sidebar from '../Sidebar'
@@ -7,6 +7,35 @@ import './Home.css';
 
 class Reports extends Component {
     render() {
+
+        //print report
+        function createPDF() {
+            // get elements of report data
+            var cv = document.getElementById("cv").innerHTML;
+            var style = "<style>";
+            style =
+            style + "table {width: 100%;font: 17px Calibri;} body{font-size:12px}";
+            style =
+            style +
+            "table, th, td {border: solid 1px #DDD;color: black ;border-collapse: collapse;";
+            style = style + "padding: 2px 3px;text-align: center;}";
+            style = style + "</style>";
+        
+            // CREATE A WINDOW OBJECT.
+            var win = window.open("", "", "height=700,width=700");
+            win.document.write(
+            '<html><head><link rel="stylesheet" href="./css/manager-add-style.css" />'
+            );
+            win.document.write("<title>Report</title>"); // <title> FOR PDF HEADER.
+            win.document.write(style); // ADD STYLE INSIDE THE HEAD TAG.
+            win.document.write("</head>");
+            win.document.write(cv);
+            // THE TABLE CONTENTS INSIDE THE BODY TAG.
+            win.document.write("</body></html>");
+            win.document.close(); // CLOSE THE CURRENT WINDOW.
+            win.print(); // PRINT THE CONTENTS.
+        }
+
         return ( 
             <div>
                 {/* ----------navigation bar inserted---------------- */}
@@ -52,17 +81,19 @@ class Reports extends Component {
                             </CardGroup>
 
                             {/* ---------overall report import from power bi --------- */}
-                            <iframe className="mt-3" width="1200" height="550" src="https://app.powerbi.com/reportEmbed?reportId=5c5866ba-ee23-4c28-a43d-dd967eef36ff&autoAuth=true&ctid=aa232db2-7a78-4414-a529-33db9124cba7&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
-                            
+                            <Container fluid id = "cv" >
+                                <iframe className="mt-3" width="100%" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=5c5866ba-ee23-4c28-a43d-dd967eef36ff&autoAuth=true&ctid=aa232db2-7a78-4414-a529-33db9124cba7&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+                            </Container>
+
                             {/*------- button group -------- */}
-                            {/* <div className="row form-group mx-3 mt-2 formGroup">
+                            <div className="row form-group mx-3 mt-2 formGroup">
                                 <div className='col text-center'>
-                                    <button name="Save" value="Save" type="submit" className="btn btn-primary custom-btn1 ml-1">SAVE</button>
+                                    <button name="Save" onClick={createPDF} value="Save" type="submit" className="btn btn-primary custom-btn1 ml-1">SAVE</button>
                                     <Link to="/reports">
                                         <button name="Cancel" value="Cancel" type="submit" className="btn btn-primary custom-btn2 ml-1">CANCEL</button>
                                     </Link>
                                 </div>
-                            </div> */}
+                            </div>
                         
                         </div>
                     </div>
--- src/components/reports/clientreport.js
+++ src/components/reports/clientreport.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Card, CardGroup } from 'react-bootstrap'
+import {Card, CardGroup, Container } from 'react-bootstrap'
 import Homenav from '../Homenav';
 import Sidebar from '../Sidebar';
 
 function ClientReport() {
+
+    //print report
+    function createPDF() {
+        // get elements of report data
+        var cv = document.getElementById("cv").innerHTML;
+        var style = "<style>";
+        style =
+          style + "table {width: 100%;font: 17px Calibri;} body{font-size:12px}";
+        style =
+          style +
+          "table, th, td {border: solid 1px #DDD;color: black ;border-collapse: collapse;";
+        style = style + "padding: 2px 3px;text-align: center;}";
+        style = style + "</style>";
+      
+        // CREATE A WINDOW OBJECT.
+        var win = window.open("", "", "height=700,width=700");
+        win.document.write(
+          '<html><head><link rel="stylesheet" href="./css/manager-add-style.css" />'
+        );
+        win.document.write("<title>Report</title>"); // <title> FOR PDF HEADER.
+        win.document.write(style); // ADD STYLE INSIDE THE HEAD TAG.
+        win.document.write("</head>");
+        win.document.write(cv);
+        // THE TABLE CONTENTS INSIDE THE BODY TAG.
+        win.document.write("</body></html>");
+        win.document.close(); // CLOSE THE CURRENT WINDOW.
+        win.print(); // PRINT THE CONTENTS.
+      }
+
     return ( 
         <div>
                 {/*-------- Account Officer navigation bar ---------- */}
@@ -46,17 +75,19 @@ function ClientReport() {
                         </CardGroup>
 
                         {/* ---------client payments report import from power bi --------- */}
-                        <iframe className="mt-3" width="1200" height="550" src="https://app.powerbi.com/reportEmbed?reportId=2bf71ba9-f9ac-4be9-ba4d-0402e6e758db&autoAuth=true&ctid=aa232db2-7a78-4414-a529-33db9124cba7&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+                        <Container fluid id = "cv" >
+                            <iframe className="mt-3" width="100%" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=2bf71ba9-f9ac-4be9-ba4d-0402e6e758db&autoAuth=true&ctid=aa232db2-7a78-4414-a529-33db9124cba7&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+                        </Container>
                         
                         {/*------- button group -------- */}
-                        {/* <div className="row form-group mx-3 mt-2 formGroup">
+                        <div className="row form-group mx-3 mt-2 formGroup">
                             <div className='col text-center'>
-                                <button name="Save" value="Save" type="submit" className="btn btn-primary custom-btn1 ml-1">SAVE</button>
+                                <button name="Save" onClick={createPDF} value="Save" type="submit" className="btn btn-primary custom-btn1 ml-1">SAVE</button>
                                 <Link to="/reports">
                                     <button name="Cancel" value="Cancel" type="submit" className="btn btn-primary custom-btn2 ml-1">CANCEL</button>
                                 </Link>
                             </div>
-                        </div> */}
+                        </div>
                     </div>
                 </div>
             </div>
